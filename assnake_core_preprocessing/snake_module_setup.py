8a1d2f5dcf6f91fecacc3f458668665e20e00eb2
Fedorov113 <fedorov.de@gmail.com>
2020-01-28 11:36:51 +0300

    Initial commit

---
+++ assnake_core_preprocessing/snake_module_setup.py
@@ -0,0 +1,13 @@
+import os
+from assnake.api.snake_module import SnakeModule
+from assnake_core_preprocessing.count.cmd_count import count_invocation
+from assnake_core_preprocessing.trimmomatic.cmd_trimmomatic import trimmomatic_invocation
+from assnake.utils import read_yaml
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+snake_module = SnakeModule(name = 'assnake-core-preprocessing', 
+                           install_dir = this_dir,
+                           snakefiles = ['./count/count.py', './trimmomatic/trimmomatic.py'],
+                           invocation_commands = [count_invocation, trimmomatic_invocation],
+                           wc_configs = [read_yaml(os.path.join(this_dir, './count/wc_config.yaml')),
+                            read_yaml(os.path.join(this_dir, './trimmomatic/wc_config.yaml'))])
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from assnake.utils import get_config_loc, load_config_file
+import os, shutil
+
+def prepare_params():
+    if os.path.isfile(get_config_loc()):
+        config = load_config_file()
+        os.makedirs(os.path.join(config['assnake_db'], 'params/tmtic'), exist_ok=True)
+        shutil.copyfile('./assnake_core_preprocessing/trimmomatic/def.json', os.path.join(config['assnake_db'], 'params/tmtic/def.json'))
+        shutil.copytree('./assnake_core_preprocessing/trimmomatic/adapters',os.path.join(config['assnake_db'], 'params/tmtic/adapters'))
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        prepare_params()
+        develop.run(self)
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        prepare_params()
+        install.run(self)
+
+
+setup(
+    name='assnake-core-preprocessing',
+    version='0.0.1',
+    packages=find_packages(),
+    entry_points = {
+        'assnake.plugins': ['assnake-core-preprocessing = assnake_core_preprocessing.snake_module_setup:snake_module']
+    },
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    }
+)
