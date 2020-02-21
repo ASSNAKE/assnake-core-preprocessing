24dd77fe70296a43c67ac50211bc46a82326b3f4
albigel <dtumandda@gmail.com>
2020-02-21 07:29:25 +0300

    add multiqc, improve ux

---
+++ assnake_core_preprocessing/multiqc/cmd_multiqc.py
@@ -0,0 +1,41 @@
+import click
+from assnake.utils import read_yaml, pathizer
+import datetime
+from assnake_core_preprocessing.utils import format_cmdinp2obj, prepare_fastqc_list_multiqc
+import os
+from pathlib import Path
+
+@click.command('multiqc', short_help='Forming the multiqc report')
+@click.option('--df', '-d', help='Name of the dataset', required=True)
+@click.option('--preproc', '-p', help='Preprocessing to use')
+@click.option('--samples-to-add', '-s',
+              help='Samples from dataset to process',
+              default='',
+              metavar='<samples_to_add>',
+              type=click.STRING)
+@click.option('--set-name', '-n', help='Name of the set', required=False, type=click.STRING )
+@click.pass_obj
+# DONE <opt parameter>
+# DONE format line 31;pass to res_list
+# TODO 22-29 to function, plus result_wc in signature; plus how-to format in signature : options [sample, sample_file, custom].
+
+def multiqc_invocation(config, df, preproc, samples_to_add, set_name):
+    multiqc_report_wc = read_yaml( Path(__file__).parent.absolute() / 'wc_config.yaml' )['multiqc_report_wc']
+    ss, df_loaded = format_cmdinp2obj(samples_to_add=samples_to_add, config=config, preproc=preproc, df=df)
+    if set_name == '':
+        curr_date = datetime.datetime.now()
+        def_name = '{df}_{preproc}_{month}{year}'.format(df=df, preproc=preproc, month=curr_date.strftime("%b"), year=curr_date.strftime("%y"))
+        set_name = def_name
+    prepare_fastqc_list_multiqc(sample_setObj=ss, set_name=set_name, strand='R1')
+    prepare_fastqc_list_multiqc(sample_setObj=ss, set_name=set_name, strand='R2')
+
+    res_list = [multiqc_report_wc.format(fs_prefix=df_loaded['fs_prefix'], df=df_loaded['df'], sample_set=set_name,
+                                         strand='R1'),
+                multiqc_report_wc.format(fs_prefix=df_loaded['fs_prefix'], df=df_loaded['df'], sample_set=set_name,
+                                         strand='R2')]
+    if config.get('requests', None) is None:
+        config['requests'] = res_list
+    else:
+        config['requests'] += res_list
+
+
--- assnake_core_preprocessing/multiqc/multiqc.py
+++ assnake_core_preprocessing/multiqc/multiqc.py
@@ -1,16 +1,18 @@
 def get_fastqc_files_for_multiqc(wildcards):
     fastqc_files = []
-    with open('{prefix}/{df}/multiqc/{sample_set}/{strand}/samples.list'.format(**wildcards), 'r') as sample_list:
+    with open('{fs_prefix}/{df}/multiqc/{sample_set}/{strand}/samples.list'.format(**wildcards), 'r') as sample_list:
         fastqc_files = [s.strip() for s in sample_list.readlines()]
     return fastqc_files
 
 rule multiqc_fastqc:
     input:
-        sample_list = '{prefix}/{df}/multiqc/{sample_set}/{strand}/samples.list',
-        # samples = get_fastqc_files_for_multiqc
+        sample_list = wc_config['sample_list_wc'],
+        samples = get_fastqc_files_for_multiqc
     output:
-        multiqc_report = '{prefix}/{df}/multiqc/{sample_set}/{strand}/multiqc_report.html'
+        multiqc_report = wc_config['multiqc_report_wc']
     params:
-        wd = '{prefix}/{df}/multiqc/{sample_set}/{strand}/'
+        wd = wc_config['work_dir_wc']
     conda: "multiqc.yaml"
     shell: ("export LC_ALL=en_US.UTF-8; export LANG=en_US.UTF-8; multiqc --file-list {input.sample_list} -o {params.wd}")
+
+
