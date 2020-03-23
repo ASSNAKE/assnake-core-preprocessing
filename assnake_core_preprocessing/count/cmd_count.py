570cbefb65e292ad92e4a7de35bbb209c2cd1ed8
Fedorov113 <fedorov.de@gmail.com>
2020-03-23 16:32:56 +0300

    Update cmd_count.py

--- assnake_core_preprocessing/count/cmd_count.py
+++ assnake_core_preprocessing/count/cmd_count.py
@@ -3,60 +3,14 @@ import assnake
 from tabulate import tabulate
 import click, os
 import pandas as pd
+from assnake.cli.cli_utils import sample_set_construction_options, add_options, generic_command_individual_samples, generate_result_list
 
 @click.command('count', short_help='Count number of reads and basepairs in fastq file')
-
-@click.option('--df','-d', help='Name of the dataset', required=True )
-@click.option('--preproc','-p', help='Preprocessing to use' )
-
-@click.option('--meta-column', '-c', help='Select samples based on metadata column' )
-@click.option('--column-value','-v', help='Value of metadata column by which select samples' )
-
-@click.option('--samples-to-add','-s', 
-                help='Samples from dataset to process', 
-                default='', 
-                metavar='<samples_to_add>', 
-                type=click.STRING )
-
+@add_options(sample_set_construction_options)
 @click.pass_obj
 
-def count_invocation(config, df, preproc, meta_column, column_value, samples_to_add):
-    samples_to_add = [] if samples_to_add == '' else [c.strip() for c in samples_to_add.split(',')]
-    df = assnake.api.loaders.load_df_from_db(df)
-
-    meta_loc = os.path.join(df['fs_prefix'], df['df'], 'mg_samples.tsv')
-    if os.path.isfile(meta_loc):
-        meta = pd.read_csv(os.path.join(df['fs_prefix'], df['df'], 'mg_samples.tsv'), sep = '\t')
-
-        if meta_column is not None and column_value is not None:
-            subset_by_col_value = meta.loc[meta[meta_column] == column_value]
-            if len(subset_by_col_value) > 0:
-                samples_to_add = list(subset_by_col_value['new_sample_name'])
-
-    
-
-    ss = assnake.SampleSet.SampleSet(df['fs_prefix'], df['df'], preproc, samples_to_add=samples_to_add)
-    config['requested_dfs'] += [df['df']] 
-
-    click.echo(tabulate(ss.samples_pd[['fs_name', 'reads', 'preproc']].sort_values('reads'), headers='keys', tablefmt='fancy_grid'))
-
-    res_list = []
-    for s in ss.samples_pd.to_dict(orient='records'):
-        if preproc == '':
-            preprocessing = s['preproc']
-        else:
-            preprocessing = preproc
-        for strand in ['R1', 'R2']:
-            res_list.append(config['wc_config']['count_wc'].format(
-                fs_prefix = s['fs_prefix'].rstrip('\/'),
-                df = s['df'],
-                preproc = s['preproc'],
-                sample = s['fs_name'],
-                strand = strand
-            )) 
-
-
-    if config.get('requests', None) is None:
-        config['requests'] = res_list
-    else:
-        config['requests'] += res_list
+def count_invocation(config, **kwargs):
+    wc_str = config['wc_config']['count_wc'] 
+    kwargs.update({'strand': 'R1'})
+    sample_set, sample_set_name = generic_command_individual_samples(config,  **kwargs)
+    config['requests'] += generate_result_list(sample_set, wc_str, **kwargs)
