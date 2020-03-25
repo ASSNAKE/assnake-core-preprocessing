ce010866298e5cbba7c36fe446536332a28e5421
Fedorov113 <fedorov.de@gmail.com>
2020-03-25 14:11:55 +0300

    Update cmd_trimmomatic.py
    to new generics

--- assnake_core_preprocessing/trimmomatic/cmd_trimmomatic.py
+++ assnake_core_preprocessing/trimmomatic/cmd_trimmomatic.py
@@ -1,33 +1,17 @@
 import assnake.api.loaders
 import assnake
-from tabulate import tabulate
 import click, glob
+from assnake.cli.cli_utils import sample_set_construction_options, add_options, generic_command_individual_samples, generate_result_list
 
 parameters = [p.split('/')[-1].replace('.json', '') for p in glob.glob('/data11/bio/databases/ASSNAKE/params/tmtic/*.json')]
 
 @click.command('trimmomatic', short_help='Quality based trimming')
-
-@click.option('--df','-d', help='Name of the dataset', required=True )
-@click.option('--preproc','-p', help='Preprocessing to use' )
-@click.option('--samples-to-add','-s', 
-                help='Samples from dataset to process', 
-                default='', 
-                metavar='<samples_to_add>', 
-                type=click.STRING )
 @click.option('--params', help='Parameters id to use. Available parameter sets: ' + str(parameters), required=False, default = 'def')
-
-
+@add_options(sample_set_construction_options)
 @click.pass_obj
 
-def trimmomatic_invocation(config, df, preproc, samples_to_add, params):
-    samples_to_add = [] if samples_to_add == '' else [c.strip() for c in samples_to_add.split(',')]
-    df = assnake.api.loaders.load_df_from_db(df)
-    ss = assnake.SampleSet.SampleSet(df['fs_prefix'], df['df'], preproc, samples_to_add=samples_to_add)
-
-    click.echo(tabulate(ss.samples_pd[['fs_name', 'reads', 'preproc']].sort_values('reads'), 
-        headers='keys', tablefmt='fancy_grid'))
-    res_list = ss.get_locs_for_result('trimmomatic', params=params)
-    if config.get('requests', None) is None:
-        config['requests'] = res_list
-    else:
-        config['requests'] += res_list
+def trimmomatic_invocation(config, params, **kwargs):
+    wc_str = '{fs_prefix}/{df}/reads/{preproc}__tmtic_{params}/{sample}_R1.fastq.gz'
+    kwargs.update({'params': params})
+    sample_set, sample_set_name = generic_command_individual_samples(config,  **kwargs)
+    config['requests'] += generate_result_list(sample_set, wc_str, **kwargs)
