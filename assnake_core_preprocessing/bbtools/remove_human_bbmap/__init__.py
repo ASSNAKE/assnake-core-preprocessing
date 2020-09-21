ac5d2966ec5aded4d0c2227fe8c345f0e86586b8
Dmitry Fedorov <fedorov.de@gmail.com>
2020-09-21 15:37:41 +0300

    rm human bbmap

--- assnake_core_preprocessing/bbtools/remove_human_bbmap/__init__.py
+++
@@ -1,1 +0,0 @@
-from assnake_core_preprocessing.bbtools.remove_human_bbmap.result import result
---
+++ assnake_core_preprocessing/remove_human_bbmap/result.py
@@ -0,0 +1,11 @@
+import click, glob, os
+from assnake.core.sample_set import generic_command_individual_samples, generate_result_list
+from assnake.core.command_builder import sample_set_construction_options, add_options
+from assnake.core.result import Result
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+result = Result.from_location(name = 'remove-human-bbmap', 
+                        location = this_dir, 
+                        description='Human DNA removal',
+                              result_type = 'preprocessing',
+                              input_type='illumina_sample',)
